2088e62c21edd685f16773f7663ba8356168a83f
harmolipi <nikob381@gmail.com>
2021-10-10 04:17:02 -0400

    Created #index and #show in KittensController

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,15 @@
 class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(kitten_params[:id])
+  end
+
+  private
+
+  def kitten_params
+    params.permit(:id, :name, :age, :cuteness, :softness)
+  end
 end
