56f0669527d3b236c7506e11458c057b6380c171
harmolipi <nikob381@gmail.com>
2021-10-10 17:47:58 -0400

    Added basic kitten model validations

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,6 @@
 class Kitten < ApplicationRecord
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :cuteness, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+  validates :softness, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
 end
