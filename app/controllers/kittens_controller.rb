a7af46741f936a7ef10df2272bee358673b9268c
harmolipi <nikob381@gmail.com>
2021-10-10 17:03:36 -0400

    Added #new, #create, #edit, and #update

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -4,12 +4,38 @@ class KittensController < ApplicationController
   end
 
   def show
-    @kitten = Kitten.find(kitten_params[:id])
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      redirect_to @kitten
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update(kitten_params)
+      redirect_to @kitten
+    else
+      render :index
+    end
   end
 
   private
 
   def kitten_params
-    params.permit(:id, :name, :age, :cuteness, :softness)
+    params.require(:kitten).permit(:id, :name, :age, :cuteness, :softness)
   end
 end
